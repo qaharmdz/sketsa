6bf6a78a5a19e2442f120612ddb473e5dd4c927a
Mudzakkir <707089+qaharmdz@users.noreply.github.com>
2018-06-14 23:38:04 +0700

    starter code

---
+++ README.md
@@ -0,0 +1,13 @@
+Sketsa is an UIkit test for various admin elements; no plan to develop it into full whistle UIkit admin template.
+
+### Development Principal
+
+- Focus on experimenting features, not performance.
+- Easy to update, develop and customize.
+    - Use default UIkit to keep it simple on update (drop-replace).
+    - Add extra UIkit stylesheet as minimum as possible.
+- CSS -class- prefix.
+    - UIkit `uk-`.
+    - UIkit extra `ux-`, extra components and modifier.
+    - Theme stylesheet `th-`.
+    - Javascript event target `js-`, should not have any style.
---
+++ assets/js/theme.js
@@ -0,0 +1,61 @@
+
+/*
+ * Load page
+ */
+var page = getUrlParameter('page', 'dashboard');
+
+$('.th-sidebar [data-page]').on('click', function(e) {
+    openPage($(this).data('page'));
+});
+function openPage(page) {
+    $('.th-sidebar a').each(function() {
+        $(this).parent().removeClass('uk-active');
+    });
+
+    // load page
+    var holder = $('[data-content]');
+
+    holder.html('<div class="uk-card uk-card-default uk-card-body uk-card-small">loading..</div>');
+    setTimeout(function() {
+        holder.load('page/' + page + '.html', function(){
+            $('.th-sidebar [data-page="' + page + '"]').parent().addClass('uk-active');
+        });
+    }, 250);
+}
+
+/*
+ * Sidebar state
+ */
+var sidebarState = Cookies.get('th-sidebar-state');
+if (sidebarState === undefined || sidebarState === 'show') {
+    UIkit.toggle($('[data-sidebar]')).toggle();
+}
+$('[data-sidebar]').on('click', function(e) {
+    setTimeout(function() {
+        if ($('body').hasClass('th-sidebar-show')) {
+            Cookies.set('th-sidebar-state', 'show');
+        } else {
+            Cookies.set('th-sidebar-state', 'hide');
+        }
+    }, 50);
+});
+
+/*
+ * https://stackoverflow.com/a/21903119
+ */
+function getUrlParameter(sParam, sDefault) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+
+    return sDefault;
+}
